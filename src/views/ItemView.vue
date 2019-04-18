edab8966ea39dc8bf98ed942af6ea3d2e9ba0f3e
sunginHwang <gommpo111@gmail.com>
2019-04-18 19:31:49 +0900

    1. studying vue component life-cycle

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
+    <div class="item-container">
         <section>
-            <div class="user-contaienr">
+            <div class="user-container">
                 <UserProfile>
                     <router-link slot="userName" :to="`/user/${fetchedItem.user}`">
                         {{fetchedItem.user}}
                     </router-link>
                     <template slot="time">{{ 'posted ' + fetchedItem.time_ago}}</template>
                 </UserProfile>
+                <div class="item-button-area">
+                    <button class="item-button" @click="getItems(19669517)">이전</button>
+                    <button class="item-button" @click="getItems(19687980)">이후</button>
+                </div>
             </div>
         </section>
         <section>
@@ -28,24 +32,61 @@ export default {
     components:{
         UserProfile
     },
+    data() {
+        return {
+            itemIdData: 0
+        }
+    },
     computed:{
         ...mapGetters('item',{
             fetchedItem: 'fetchedItem'
         })
     },
+    methods: {
+        getItems(itemId) {
+            console.log(`getItems : ${itemId}`);
+            this.itemIdData = itemId;
+            this.$store.dispatch('item/FETCH_ITEM',itemId);
+        }
+    },
+    beforeUpdate(){
+        console.log(`beforeUpdate : ${this.itemIdData}`);
+    },
+    updated(){
+        console.log(`updated : ${this.itemIdData}`);
+
+    },
+    mounted(){
+        console.log(`mounted : ${this.itemIdData}`);
+    },
     created(){
         const itemId = this.$route.params.id;
-        this.$store.dispatch('item/FETCH_ITEM',itemId);
+        console.log(`created : ${itemId}`);
+        this.getItems(itemId);
     }
 }
 </script>
 
 <style scoped>
-.user-contaienr{
-    display:flex;
-    align-items:center;
-    padding:0.5rem
+.item-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
+
+.item-button-area{
+    margin: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+}
+.item-button{
+    padding: 0.5rem;
+    width: 2.5rem;
+    border-radius: 3px;
+    border: 1px solid #41b883;
+
+}
+
 .fa-user{
     font-size: 2.5rem;
 }
