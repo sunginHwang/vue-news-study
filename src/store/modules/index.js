c2c8d0c94c1d6dcde242fbbcfc1b8b9131b37dc0
sunginHwang <gommpo111@gmail.com>
2019-06-03 07:49:26 +0900

    1. modules find index string -> reg fix

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,17 +1,12 @@
 import camelCase from 'lodash/camelCase';
 
-const requireModule = require.context('.',false, /\.js$/);
+const requireModule = require.context('.', true, /^(?!.\/index).*.js$/);
+
 const modules = {};
 
 requireModule.keys().forEach(filename => {
-
-    const isIndexJsFile = filename ===  './index.js';
-
-    if(!isIndexJsFile){
-        const moduleName = camelCase(filename.replace(/(\.\/|\.js)/g,''));
-        modules[moduleName] = {namespaced:true, ...requireModule(filename)};
-    }
-
+    const moduleName = camelCase(filename.replace(/(\.\/|\.js)/g, ''));
+    modules[moduleName] = {namespaced: true, ...requireModule(filename)};
 });
 
 export default modules;
