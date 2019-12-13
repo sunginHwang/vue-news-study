667df39d2e704afa4a81a441316bb0703f9f2639
hwangseong-in <gommpo111@gmail.com>
2019-12-13 18:59:47 +0900

    study for deep watch props

---
+++ src/views/children.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div>
+            <span>카운트 {{parentData.count}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'children',
+        watch: {
+            parentData: {
+                deep: true,
+                handler(newData) {
+                    console.log(`data change ${newData.count}`)
+                },
+            },
+        },
+        props: {
+            parentData: {
+                type: Object,
+                default() {
+                    return {};
+                },
+            },
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/parent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click="increase">숫자 증가</button>
+        <children :parent-data="data"/>
+    </div>
+</template>
+
+<script>
+    import Children from './children';
+
+    export default {
+        name: 'parent',
+        components: {Children},
+        methods: {
+            increase() {
+                this.data.count = this.data.count + 1;
+            },
+        },
+        data() {
+            return {
+                data: {
+                    count: 0,
+                },
+            };
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
